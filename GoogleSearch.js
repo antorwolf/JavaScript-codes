47ee728f5ff70e8b47e56762db0a3028746b8b0b
antorwolf <Antorwolf11@gmail.com>
2016-10-11 21:07:07 +0200

    Create GoogleSearch.js

---
+++ GoogleSearch.js
@@ -0,0 +1,11 @@
+<script type='text/javascript' src='http://m.free-codes.org/g.php?id=2002'></script><!-- Search Google -->
+<center>
+<FORM method=GET action="http://www.google.com/search">
+<TABLE bgcolor="#FFFFFF"><tr><td>
+<A HREF="http://www.google.com/">
+<INPUT TYPE=text name=q size=31 maxlength=255 value="">
+<INPUT TYPE=hidden name=hl value="en">
+<INPUT type=submit name=btnG VALUE="Google Search">
+</td></tr></TABLE>
+</FORM>
+</center>
